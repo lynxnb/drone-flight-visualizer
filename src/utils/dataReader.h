847e00ab9b915537c521b34e35f5f7d307517c30
zibas <milo.brontesi@gmail.com>
2023-08-30 16:35:15 +0200

    generic ReadFile function

--- src/utils/dataReader.h
+++ src/utils/dataReader.h
@@ -7,4 +7,5 @@ namespace dfv::utils {
     std::vector<objects::flight_data_point> ReadFlightData(const std::string &csvPath);
     void PopulateElevation(std::vector<objects::node> &nodes);
     objects::osm_data FetchOSMData(const std::string &bbox);
+    static std::vector<char> ReadFile(const std::string &filename);
 }
