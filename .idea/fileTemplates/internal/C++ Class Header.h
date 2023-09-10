458af3e39daf6fe5d8f8502f80b4c6381871f77a
lynxnb <niccolo.betto@gmail.com>
2023-09-10 18:08:28 +0200

    Update IDE file templates

---
+++ .idea/fileTemplates/internal/C++ Class Header.h
@@ -0,0 +1,8 @@
+#parse("C File Header.h")
+#[[#pragma]]# once
+
+${NAMESPACES_OPEN}
+class ${NAME} {
+
+};
+${NAMESPACES_CLOSE}
---
+++ .idea/fileTemplates/internal/C++ Class.cc
@@ -0,0 +1,5 @@
+#parse("C File Header.h")
+#[[#include]]# "${HEADER_FILENAME}"
+
+${NAMESPACES_OPEN}
+${NAMESPACES_CLOSE}
