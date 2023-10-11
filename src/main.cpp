7bce0fcff26b10488238f18f50d044c8b41da912
lynxnb <niccolo.betto@gmail.com>
2023-10-11 19:38:08 +0200

    Use a try-catch block in main entrypoint to allow catching exceptions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <chrono>
+#include <iostream>
 
 #include "utils/data_reader.h"
 #include "vulkan/vk_engine.h"
@@ -9,7 +9,7 @@ using namespace dfv;
 namespace {
     using clock = std::chrono::steady_clock;
     using time_point = std::chrono::time_point<clock>;
-}
+} // namespace
 
 /*
  * Command line usage: drone_flight_visualizer $1
@@ -19,11 +19,13 @@ int main(int argc, char **argv) {
     std::cout << "drone_flight_visualizer" << std::endl;
 
     VulkanEngine engine;
-    engine.init();
-
-    engine.run();
+    try {
+        engine.init();
+        engine.run();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
 
     engine.cleanup();
-
     return 0;
 }
