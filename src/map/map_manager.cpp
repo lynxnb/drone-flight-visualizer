2fc5bd8e7ed8a78b1d35b6d52a27a92b244b03c3
lynxnb <niccolo.betto@gmail.com>
2023-12-15 17:17:00 +0100

    Update map manager to use flight data bounding box

--- src/map/map_manager.cpp
+++ src/map/map_manager.cpp
@@ -5,22 +5,25 @@
 namespace dfv {
     Mesh MapManager::initialize(FlightData &flightData) {
         const auto &drone_path = flightData.getPath();
-        dfv::structs::DiscreteBox box = {}; // Example values
+
+        auto bbox = flightData.getBoundingBox();
+
+        dfv::structs::DiscreteBox box = {.llLat = bbox.llLat,
+                                         .llLon = bbox.llLon,
+                                         .urLat = bbox.urLat,
+                                         .urLon = bbox.urLon,
+                                         .spacingMeters = 0};
+
         for (const auto &point : drone_path) {
-            if (point.z == 0 || point.x == 0) {
-                std::cout << "found null: " << point.x << point.z << std::endl;
-            }
-            box.llLat = std::min(box.llLat, double(point.x));
-            box.llLon = std::min(box.llLon, double(point.z));
-            box.urLat = std::max(box.urLat, double(point.x));
-            box.urLon = std::max(box.urLon, double(point.z));
-            this->drone_path.push_back(structs::Node(double(point.x), double(point.z), double(point.y)));
+            this->drone_path.emplace_back(static_cast<double>(point.x),
+                                          static_cast<double>(point.z),
+                                          static_cast<double>(point.y));
         }
 
-        box.llLat -= 5;
-        box.llLon -= 5;
-        box.urLat += 5;
-        box.urLon += 5;
+        box.llLat -= 0.01;
+        box.llLon -= 0.01;
+        box.urLat += 0.01;
+        box.urLon += 0.01;
 
         for (auto el : drone_path) {
             this->drone_path.emplace_back(el.x, el.z, el.y);
@@ -28,7 +31,7 @@ namespace dfv {
 
         // Call the createGrid method with some debug values
         double sparsity = 4; // Example density
-        double box_size = 2; // Example box size
+        double box_size = 0.002; // Example box size
         double node_density_coefficient = 0.8; // Example coefficient
 
         std::vector<std::vector<dfv::structs::DiscreteBoxInfo>> box_matrix = dfv::map::createGrid(box, this->drone_path, sparsity, box_size, node_density_coefficient);
